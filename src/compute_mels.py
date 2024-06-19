7bca4b1af1a06d282863fb9083c8261e2e108db8
ChemrovKirill <chemrovsk@mail.ru>
2024-06-19 10:17:16 +0300

    move compute mels to src

--- src/compute_mels.py
+++ src/compute_mels.py
@@ -16,10 +16,14 @@ def mel(arr, sr=32_000):
 
 if __name__ == "__main__":
 
+    train_csv_path = 'tmp/train_noduplicates.csv'
+    train_path = 'data/train_audio/'
+    train_output_path = 'tmp/train_mels/'
+
     print("Creating mel spectrograms from train_audio...")
-    data = pd.read_csv('data/data_enriched_no_duplicates.csv')
-    data['path'] = 'data/train_audio/' + data["filename"]
-    data['mel_path'] = 'data/train_mels/' + data["filename"].apply(lambda s: s.replace('.ogg', '.npy'))
+    data = pd.read_csv(train_csv_path)
+    data['path'] = train_path + data["filename"]
+    data['mel_path'] = train_output_path + data["filename"].apply(lambda s: s.replace('.ogg', '.npy'))
 
     def process_train(idx):
         row = data.iloc[idx]
@@ -28,30 +32,34 @@ if __name__ == "__main__":
         os.makedirs('/'.join(row['mel_path'].split('/')[:-1]), exist_ok=True)
         np.save(row['mel_path'], mel_spec)
 
-    indexes = data.index
+    indexes = data.index[:10]
     _ = joblib.Parallel(n_jobs=-1, backend="loky")(
             joblib.delayed(process_train)(idx) for idx in tqdm(indexes, total=len(indexes))
         )
     print("Done")
 
+    unlab_csv_path = 'tmp/train_noduplicates.csv'
+    unlab_path = 'data/unlabeled_soundscapes/'
+    unlab_output_path = 'tmp/unlabeled_mels/'
+
     print("Creating mel spectrograms from unlabeled_soundscapes...")
-    unlabeled_dir = 'data/unlabeled_soundscapes/'
-    paths = [unlabeled_dir+f for f in sorted(os.listdir(unlabeled_dir))]
+    paths = [unlab_path + f for f in sorted(os.listdir(unlab_path))]
     data_unlabeled = pd.DataFrame(paths, columns=['filepath'])
     data_unlabeled['filename'] = data_unlabeled.filepath.map(lambda x: x.split('/')[-1])
-    data_unlabeled['mel_path'] = 'data/unlabeled_mels/' + data_unlabeled["filename"].apply(lambda s: s.replace('.ogg', '.npy'))
+    data_unlabeled['mel_path'] = unlab_output_path + data_unlabeled["filename"].apply(lambda s: s.replace('.ogg', '.npy'))
 
     def process_unlabeled(idx):
         row = data_unlabeled.iloc[idx]
         audio, sr = librosa.load(row['filepath'], sr=None)
         mel_spec = mel(audio, sr=sr)
         os.makedirs('/'.join(row['mel_path'].split('/')[:-1]), exist_ok=True)
+        # saving 1 minute per file to speed up reading during training
         for i in range(4):
             start = i * 320 * 12
             end = (i + 1) * 320 * 12
             np.save(row['mel_path'].replace('.npy', f'_{i}.npy'), mel_spec[:, start:end])
 
-    indexes = data_unlabeled.index
+    indexes = data_unlabeled.index[:10]
     _ = joblib.Parallel(n_jobs=-1, backend="loky")(
             joblib.delayed(process_unlabeled)(idx) for idx in tqdm(indexes, total=len(indexes))
         )
