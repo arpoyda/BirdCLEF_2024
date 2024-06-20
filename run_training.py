d8454ba15aa5ad456a5c028e7eb9771355a2602e
arpoyda <arpoyda@github.com>
2024-06-20 20:10:33 +0300

    run training + args

--- run_training.py
+++ run_training.py
@@ -1,4 +1,13 @@
 import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-с', '--cfg', help='Model config file path', dest='cfg_path')
+args = {}
+for name, value in vars(parser.parse_args()).items():
+    args[name] = value
+
 
 print('======= TRAINING =======')
-os.system(f"python ./src/training.py -c")
+os.system(f"python ./src/training.py -c {args['cfg_path']}")
